8e1b8810d53dac5d99b8a67139db24cbff9d3f99
João Pedro Limão <jplimao12@gmail.com>
2020-06-22 18:56:02 -0300

    Level#08 EX#01

---
+++ Level#08/EX#01.go
@@ -0,0 +1,41 @@
+package main
+// Using Marshal to encode users in JSON
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type User struct {
+	First string
+	Age   int
+}
+
+func main() {
+	u1 := user{
+		First: "James",
+		Age:   32,
+	}
+
+	u2 := user{
+		First: "Moneypenny",
+		Age:   27,
+	}
+
+	u3 := user{
+		First: "M",
+		Age:   54,
+	}
+
+	users := []user{u1, u2, u3}
+
+	fmt.Println(users)
+
+	sb, err := json.Marshal(users)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(sb))
+
+}
